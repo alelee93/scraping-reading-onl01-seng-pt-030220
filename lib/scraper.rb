8570b400a37a584be4e0ed1a274da9fed2309716
alelee93 <leevargasale@gmail.com>
2020-04-06 23:44:38 -0500

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,16 @@
+require 'pry'
 require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+##pp doc.css(".headline-26OIBN").text
+courses = doc.css("#3c239880-48cf-5ce7-84d0-578546d87586 .inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
+#binding.pry
+
+courses.each do |course|
+    puts course.text.strip
+  end
+
+  p doc.css("#3c239880-48cf-5ce7-84d0-578546d87586 .inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")[0]
+
